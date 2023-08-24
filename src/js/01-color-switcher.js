851711aa6659d11dc80eff23ebd84bdbc9200d9c
krystianklimczak <krystianklimczak1996r.com>
2023-08-24 21:05:01 +0200

    implemented bg color changer app

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,36 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
+const btn = document.querySelectorAll('button');
 let timerId = null;
 const delay = 1000;
 
+// disabled stop btn at the beggining
 disablingStopBtn();
 
+// function to get random color in hex
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+// function disabling start btn
 function disablingStartBtn() {
   startBtn.disabled = true;
   stopBtn.disabled = false;
 }
 
+// function disabling stops btn
 function disablingStopBtn() {
   startBtn.disabled = false;
   stopBtn.disabled = true;
 }
 
+// function setting background color of body
 function setBodyBgRandomColor() {
   body.style.backgroundColor = getRandomHexColor();
 }
 
+// listener for start btn
 startBtn.addEventListener('click', () => {
   setBodyBgRandomColor();
   timerId = setInterval(() => {
@@ -32,19 +39,21 @@ startBtn.addEventListener('click', () => {
   disablingStartBtn();
 });
 
+// listener for stop btn
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
   disablingStopBtn();
 });
 
-// inline styles
+// add some inline styles (unnessesary)
 body.style.display = 'flex';
 body.style.flexDirection = 'column';
+body.style.justifyContent = 'center';
 body.style.alignItems = 'center';
-// body.style.justifyItems = 'center';
+body.style.margin = 'auto';
 body.style.gap = '15px';
+body.style.height = '100vh';
 
-const btn = document.querySelectorAll('button');
 btn.forEach(e => {
-  e.style.padding = '5px 15px';
+  e.style.padding = '15px 25px';
 });
